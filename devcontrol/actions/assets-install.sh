dd09ffec43ef556b8bade4a1037b27062cb16027
Pedro Amador <pedroamador.rodriguez@gmail.com>
2020-01-27 06:53:21 +0000

    New: Add assets-install devcontrol action

---
+++ devcontrol/actions/assets-install.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+set -eu
+
+# Initialize
+cd "$(dirname "$0")/../.."
+source config.ini
+
+# @description Install item assets
+#
+# @example
+#   assets-install
+#
+# @arg $1 Task: "brief", "help" or "exec"
+#
+# @exitcode The result of the assets installation
+#
+# @stdout "Not implemented" message if the requested task is not implemented
+#
+function assets-install() {
+
+    # Init
+    local briefMessage
+    local helpMessage
+
+    briefMessage="Install assets"
+    helpMessage=$(cat <<EOF
+Install all product and service assets:
+
+* Create all platform pieces directories with all permissions (777)
+* Create the network "platform_services"
+* Create the network "platform_products"
+EOF
+)
+
+    # Task choosing
+    case $1 in
+        brief)
+            showBriefMessage "${FUNCNAME[0]}" "$briefMessage"
+            ;;
+        help)
+            showHelpMessage "${FUNCNAME[0]}" "$helpMessage"
+            ;;
+        exec)
+            # Create network "services"
+            if [ "$(docker network ls -f name='platform_services' -q)" == "" ]; then
+                echo -n "- Creating docker network 'platform_services' ..."
+                docker network create platform_services
+                echo "[OK]"
+            else
+                echo "The 'platform_services' docker network already exists, skipping"
+            fi
+            # Create network "products"
+            if [ "$(docker network ls -f name='platform_products' -q)" == "" ]; then
+                echo -n "- Creating docker network 'platform_products' ..."
+                docker network create platform_products
+                echo "[OK]"
+            else
+                echo "The 'platform_products' docker network already exists, skipping"
+            fi
+            # Create directories
+            for directory in ${assets[*]}; do
+                if [ ! -d "${directory}" ]; then
+                    echo -n "- Creating '${directory}' directory..."
+                    mkdir -p "${directory}"
+                    echo "[OK]"
+                    echo -n "- Setting '${directory}' permissions..."
+                    chmod 777 "${directory}"
+                    echo "[OK]"
+                else
+                    echo "The '${directory}' directory already exists, skipping"
+                fi
+            done
+            ;;
+        *)
+            showNotImplemtedMessage "$1" "${FUNCNAME[0]}"
+            return 1
+    esac
+}
+
+# Main
+assets-install "$@"
